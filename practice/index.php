de9486b343f46497b41198db40b0a9953feba78f
Caroline Im <im@fas.harvard.edu>
2021-02-04 00:38:56 -0500

    Added practice application

---
+++ practice/index.php
@@ -0,0 +1,2 @@
+<!doctype html> <html lang='en'> <head>     <title>Practice Application</title>     <meta charset='utf-8'>     
+<link href=data:, rel=icon> </head> <body>     <h1>Practice Application</h1> </body> </html>
